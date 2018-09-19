34779edd519c7fd2ac0f8ac5499bf5a59dc82425
modicristian <cristianmodi@gmail.com>
2018-09-18 22:07:45 -0300

    Realizando Toolbar

--- app/src/main/java/com/max/agendasql/Interfaz/Principal.java
+++ app/src/main/java/com/max/agendasql/Interfaz/Principal.java
@@ -2,7 +2,11 @@ package com.max.agendasql.Interfaz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.max.agendasql.Listeners.ListenerRecycler;
 import com.max.agendasql.Listeners.ListenerRegistrar;
@@ -12,11 +16,14 @@ import com.max.agendasql.R;
 public class Principal extends AppCompatActivity
 {
     private Button botonRegistrar,botonVerListado,botonRecycler;
+    private android.support.v7.widget.Toolbar toolbar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
         botonRegistrar=findViewById(R.id.botonRegistrar);
         botonVerListado=findViewById(R.id.botonVerListado);
         botonRecycler=findViewById(R.id.botonRecycler);
@@ -28,5 +35,46 @@ public class Principal extends AppCompatActivity
         botonRegistrar.setOnClickListener(listenerRegistrar);
         botonVerListado.setOnClickListener(listenerVerRegistro);
         botonRecycler.setOnClickListener(listenerRecycler);
+
+        toolbar = findViewById(R.id.tolbar);
+
+        setSupportActionBar(toolbar);
+
+        getActionBar().setDisplayShowTitleEnabled(true);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setDisplayUseLogoEnabled(true);
+        getActionBar().setLogo(R.drawable.images);
+        getActionBar().setTitle("MI AGENDA");
     }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_toolbar, menu);
+        return true;
+    }
+    @Override
+        public boolean onOptionsItemSelected (MenuItem item)
+    {
+        //acciones android puras
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                break;
+        }
+        //acciones del menu definido por el desarrollador
+        switch (item.getItemId()) {
+            case R.id.agregar_cli:
+
+                Toast.makeText(this, "Agregar Cliente", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.salir:
+                super.finish();
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
 }
